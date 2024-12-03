3c2bb109b15380162bdc73d3cefc71cb205180a7
Xander Wauters <xwauters@outlook.com>
2024-12-03 12:21:05 +0100

    Added a 'speed' variable for easy control and later power-ups

--- core/src/main/java/com/Xander/shapeshifters/Player.java
+++ core/src/main/java/com/Xander/shapeshifters/Player.java
@@ -10,18 +10,38 @@ public class Player
     private Vector2 position;
     private float width;
     private float height;
+    private float speed;
 
     public Player(float x, float y, float width, float height)
     {
         this.position = new Vector2(x,y);
         this.width = width;
         this.height = height;
+        this.speed = 200;
     }
     public void render(ShapeRenderer shapeRenderer)
     {
         shapeRenderer.rect(position.x, position.y, width, height);
     }
-
+    public void update(float deltaTime)
+    {
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
+        {
+            position.x -= speed * deltaTime;  //left
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D))
+        {
+            position.x += speed * deltaTime;  //right
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W))
+        {
+            position.y += speed * deltaTime;  //up
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) ||Gdx.input.isKeyPressed(Input.Keys.S))
+        {
+            position.y -= speed * deltaTime;  //down
+        }
+    }
     public float getWidth() {
         return width;
     }
@@ -38,28 +58,16 @@ public class Player
         this.height = height;
     }
 
-    public void update(float deltaTime)
-    {
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A))
-        {
-            position.x -= 200 * deltaTime;  //left
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D))
-        {
-            position.x += 200 * deltaTime;  //right
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W))
-        {
-            position.y += 200 * deltaTime;  //up
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) ||Gdx.input.isKeyPressed(Input.Keys.S))
-        {
-            position.y -= 200 * deltaTime;  //down
-        }
-    }
-
     public Vector2 getPosition()
     {
         return position;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
     }
 }
